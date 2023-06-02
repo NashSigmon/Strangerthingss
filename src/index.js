256a84bc96f523972d3febaf7da7f3c304764160
brianreisman <brianreisman@protonmail.com>
2023-06-02 11:40:58 -0400

    remove boilerplate

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-reportWebVitals(sendToVercelAnalytics);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
